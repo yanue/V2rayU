19d6c2bb32ba3ee75c7f7e8d0a794b6fc0baafbe
yuanxue.yang <yuanxue.yang@blemobi.com>
2018-10-09 16:12:19 +0800

    Initial Commit

---
+++ V2rayU/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  V2rayU
+//
+//  Created by yanue on 2018/10/9.
+//  Copyright © 2018 yanue. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
