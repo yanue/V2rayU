b8d75337ff7a97b2c1f9922748a9dd3875722040
yuanxue.yang <yuanxue.yang@blemobi.com>
2018-10-26 20:02:08 +0800

    v2ray json struct custom encode and decode

--- V2rayU/v2ray/v2rayInbound.swift
+++ V2rayU/v2ray/v2rayInbound.swift
@@ -14,9 +14,113 @@ struct v2rayInbound: Codable  {
     var listen:String = "127.0.0.1"
     var `protocol`:v2rayProtocol = .socks
     var tag:String? = ""
-    var settings:v2rayInboundSettings?
-    var streamSettings:v2rayInboundSettings?
+    var streamSettings:v2rayStreamSettings?
     var sniffing:v2rayInboundSniffing?
+    var settingHttp:v2rayInboundHttp?
+    var settingSocks:v2rayInboundSock?
+    var settingShadowsocks:v2rayInboundShandowsocks?
+    var settingVmess:v2rayInboundVmess?
+
+    enum CodingKeys: String, CodingKey {
+        case port
+        case listen
+        case `protocol`
+        case tag
+        case streamSettings
+        case sniffing
+        case settings // auto switch
+    }
+}
+
+extension v2rayInbound {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        port = try container.decode(String.self, forKey: CodingKeys.port)
+        listen = try container.decode(String.self, forKey: CodingKeys.listen)
+        `protocol` = try container.decode(v2rayProtocol.self, forKey: CodingKeys.`protocol`)
+        tag = try container.decode(String.self, forKey: CodingKeys.tag)
+       
+        // ignore nil
+        if try !container.decodeNil(forKey: .streamSettings) {
+            streamSettings = try container.decode(v2rayStreamSettings.self, forKey: CodingKeys.streamSettings)
+        }
+        
+        // ignore nil
+        if try !container.decodeNil(forKey: .sniffing) {
+            sniffing = try container.decode(v2rayInboundSniffing.self, forKey: CodingKeys.sniffing)
+        }
+    
+        // decode settings depends on `protocol`
+        switch `protocol` {
+            case .http:
+                settingHttp = try container.decode(v2rayInboundHttp.self, forKey: CodingKeys.settings)
+                break
+            case .shadowsocks:
+                settingShadowsocks = try container.decode(v2rayInboundShandowsocks.self, forKey: CodingKeys.settings)
+                break
+            case .socks:
+                settingSocks = try container.decode(v2rayInboundSock.self, forKey: CodingKeys.settings)
+                break
+            case .vmess:
+                settingVmess = try container.decode(v2rayInboundVmess.self, forKey: CodingKeys.settings)
+                break
+            case .blackhole:
+                break
+            case .dokodemoDoor:
+                break
+            case .freedom:
+                break
+            case .mtproto:
+                break
+        }
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(port, forKey: .port)
+        try container.encode(listen, forKey: .listen)
+        try container.encode(`protocol`, forKey: .`protocol`)
+        try container.encode(tag, forKey: .tag)
+        
+        // ignore nil
+        if streamSettings != nil {
+            try container.encode(streamSettings, forKey: .streamSettings)
+        }
+        
+        // ignore nil
+        if sniffing != nil {
+            try container.encode(sniffing, forKey: .sniffing)
+        }
+        
+        // encode settings depends on `protocol`
+        switch `protocol` {
+            case v2rayProtocol.http:
+                try container.encode(self.settingHttp, forKey: .settings)
+                break
+            case .shadowsocks:
+                try container.encode(self.settingShadowsocks, forKey: .settings)
+                break
+            case .socks:
+                try container.encode(self.settingSocks, forKey: .settings)
+                break
+            case .vmess:
+                try container.encode(self.settingVmess, forKey: .settings)
+                break
+            case .blackhole:
+                break
+            case .dokodemoDoor:
+                break
+            case .freedom:
+                break
+            case .mtproto:
+                break
+        }
+    
+    }
+}
+
+struct v2rayStreamSettings: Codable {
 }
 
 struct v2rayInboundDetour: Codable {
@@ -24,8 +128,8 @@ struct v2rayInboundDetour: Codable {
     var listen:String = "127.0.0.1"
     var `protocol`:v2rayProtocol = .http
     var tag:String?
-    var settings:v2rayInboundSettings?
-    var streamSettings:v2rayInboundSettings?
+//    var settings:v2rayInboundSettings?
+//    var streamSettings:v2rayInboundSettings?
     var sniffing:v2rayInboundSniffing?
     var allocate:v2rayInboundDetourAllocate?
 }
@@ -53,21 +157,13 @@ struct proxySettings: Codable {
     var Tag: String?
 }
 
-struct v2rayInboundSettings: Codable {
-    // protocol
-    var v2rayInboundHttp:v2rayInboundHttp?
-    var v2rayProtocolShandowsock:v2rayProtocolShandowsock?
-    var v2rayInboundSock:v2rayInboundSock?
-}
-
 struct v2rayInboundHttp: Codable {
     var timeout:Int?
     var allowTransparent:Bool = false
     var userLevel:Int = 0
 }
 
-
-struct v2rayProtocolShandowsock: Codable {
+struct v2rayInboundShandowsocks: Codable {
     enum auth:String,Codable {
         case noauth
         case password
@@ -76,10 +172,10 @@ struct v2rayProtocolShandowsock: Codable {
     var udp:Bool = true
     var userLevel:Int = 0
     var timeout:Int? // 默认300
-    var accounts:[v2rayProtocolShandowsockAccount]?
+    var accounts:[v2rayInboundShandowsockAccount]?
 }
 
-struct v2rayProtocolShandowsockAccount: Codable {
+struct v2rayInboundShandowsockAccount: Codable {
     var user:String?
     var pass:String?
 }
@@ -99,6 +195,11 @@ struct v2rayInboundSock: Codable {
 }
 
 struct v2rayInboundSockAccount: Codable {
+    var user:String?
+    var pass:String?
+}
+
+struct v2rayInboundVmess: Codable {
     var user:String?
     var pass:String?
 }
--- V2rayUTests/V2rayUTests.swift
+++ V2rayUTests/V2rayUTests.swift
@@ -22,24 +22,41 @@ class V2rayUTests: XCTestCase {
 //    override func tearDown() {
 //        // Put teardown code here. This method is called after the invocation of each test method in the class.
 //    }
+    func testInbound() {
+        var inbound = v2rayInbound()
+        inbound.protocol = v2rayProtocol.vmess
+        inbound.settingHttp = v2rayInboundHttp()
+        inbound.settingSocks = v2rayInboundSock()
+        inbound.settingShadowsocks = v2rayInboundShandowsocks()
+        inbound.settingVmess = v2rayInboundVmess()
+
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted //输出格式好看点
+        let data = try! encoder.encode(inbound)
+        print(String(data: data, encoding: .utf8)!)
+
+    }
+    
     func testV2ray() {
+        
+        return
         var v2ray = v2rayStruct()
         v2ray.inbound = v2rayInbound()
         v2ray.inboundDetour = [v2rayInboundDetour()]
-        var outbound = v2rayOutbound()
+//        var outbound = v2rayOutbound()
         var vmess = v2rayOutboundVMess(
             address: "",
             port: "",
             users:[v2rayOutboundVMessUser(id:"aaa", alterId: 0, level: 0, security: "String?")]
         )
-        
-        outbound.protocol = .freedom
-        outbound.settings = v2rayOutboundSettings()
-//        outbound.settings?.Vmess_vnext = [vmess]
-        outbound.settings?.blackhole_Response = blackhole_Response()
+//
+//        outbound.protocol = .freedom
+//        outbound.settings = v2rayOutboundSettings()
+////        outbound.settings?.Vmess_vnext = [vmess]
+//        outbound.settings?.blackhole_Response = blackhole_Response()
 
-        print("outbound",outbound)
-        v2ray.outbound = outbound
+//        print("outbound",outbound)
+//        v2ray.outbound = outbound
         v2ray.outboundDetour = [v2rayOutboundDetour()]
 
         let encoder = JSONEncoder()
@@ -49,29 +66,22 @@ class V2rayUTests: XCTestCase {
 
     }
     
+    func testName() {
+        
+    }
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        
-        struct Man: Codable  {
-            var total:Int = 0
-        }
-        
-        struct Person: Codable {
-            var name: String = "aa"
-            var age: Int = 9
-            var bornIn: String?
-            var man:Man?
-        }
+       
         var ming = Person()
-        ming.age = 11
-        ming.name = "ming"
+        ming.name = "aa"
+        ming.profile1 = PersonProfile1()
         
         let encoder = JSONEncoder()
         encoder.outputFormatting = .prettyPrinted //输出格式好看点
         let data = try! encoder.encode(ming)
         print(String(data: data, encoding: .utf8)!)
-      
 
 //        print(v2ray.toJSONString(prettyPrint:true) ?? "") // 序列化为格式化后的JSON字符串
     }
