ab5cb06aec6d58add7a5944c5a9b07ebd80d6051
michaellee123 <jjjamdmin@outlook.com>
2020-05-27 00:33:49 +0800

    1、修复V2rayUHelper有可能占用大量CPU的问题；2、修复V2rayUHelper结果判断

--- V2rayU/ping.swift
+++ V2rayU/ping.swift
@@ -14,6 +14,7 @@ var fastV2rayName = ""
 var fastV2raySpeed = 5
 let pingJsonFileName = "ping.json"
 let pingJsonFilePath = AppResourcesPath + "/" + pingJsonFileName
+var task:Process?
 
 struct pingItem: Codable {
     var name: String = ""
@@ -22,9 +23,9 @@ struct pingItem: Codable {
 }
 
 class PingSpeed: NSObject {
-
+    
     var pingServers: [pingItem] = []
-
+    
     func pingAll() {
         print("ping start")
         if inPing {
@@ -35,7 +36,7 @@ class PingSpeed: NSObject {
         inPing = true
         fastV2rayName = ""
         self.pingServers = []
-
+        
         let normalTitle = menuController.statusMenu.item(withTag: 1)?.title ?? "Ping Speed..."
         let langStr = Locale.current.languageCode
         var pingTip: String = ""
@@ -53,22 +54,31 @@ class PingSpeed: NSObject {
             }
         }
         menuController.statusMenu.item(withTag: 1)?.title = pingTip
-
-
+        
+        
         let queue = DispatchQueue.global()
+        let queueInterval = DispatchQueue.global(qos: .userInitiated)
+        let interval = DispatchWorkItem{
+            print("ping terminate")
+            task?.interrupt()
+            task?.terminate()
+        }
         queue.async {
-
+            
             self.writeServerToFile()
             self.pingInCmd()
             self.parsePingResult()
-
+            
+            print("ping finish")
+            interval.cancel()
+            
             DispatchQueue.main.async {
                 menuController.statusMenu.item(withTag: 1)?.title = "\(normalTitle)"
-
+                
                 // refresh servers
                 // reload
                 V2rayServer.loadConfig()
-
+                
                 // if auto select fastest server
                 if UserDefaults.getBool(forKey: .autoSelectFastestServer) && fastV2rayName.count > 0 {
                     if V2rayServer.getIndex(name: fastV2rayName) > -1 {
@@ -80,7 +90,7 @@ class PingSpeed: NSObject {
                         menuController.startV2rayCore()
                     }
                 }
-
+                
                 // refresh server
                 menuController.showServers()
                 // reload config
@@ -90,57 +100,59 @@ class PingSpeed: NSObject {
                 inPing = false
             }
         }
+        //定时在8秒后停止V2rayUHelper（本来应该5秒自动完成的，如果没完成，就直接停止）
+        queueInterval.asyncAfter(deadline: .now() + 8,execute: interval)
     }
-
+    
     func writeServerToFile() {
         let itemList = V2rayServer.list()
         if itemList.count == 0 {
             return
         }
-
+        
         for item in itemList {
             if !item.isValid {
                 continue
             }
-
+            
             let host = self.parseHost(item: item)
             guard let _ = NSURL(string: host) else {
                 continue
             }
-
+            
             let tmp = pingItem(name: item.name, host: host, ping: item.speed)
-
+            
             self.pingServers.append(tmp)
         }
-
+        
         // 1. encode to json text
         let encoder = JSONEncoder()
         let data = try! encoder.encode(self.pingServers)
         let jsonStr = String(data: data, encoding: .utf8)!
-
+        
         try! jsonStr.data(using: String.Encoding.utf8)?.write(to: URL(fileURLWithPath: pingJsonFilePath), options: .atomic)
     }
-
+    
     func pingInCmd() {
         let cmd = "cd " + AppResourcesPath + " && chmod +x ./V2rayUHelper && ./V2rayUHelper -cmd ping -t 5s -f ./" + pingJsonFileName
-//        print("cmd", cmd)
-        let res = shell(launchPath: "/bin/bash", arguments: ["-c", cmd])
+        //        print("cmd", cmd)
+        let res = runShell(launchPath: "/bin/bash", arguments: ["-c", cmd])
         
         NSLog("pingInCmd: res=(\(String(describing: res))) cmd=(\(cmd))")
-
-        if res?.contains("ok") ?? false {
+        
+        // 这里直接判断ok有问题，res里面还有lookup
+        if res?.contains("ok config.") ?? false {
             // res is: ok config.xxxx
             fastV2rayName = res!.replacingOccurrences(of: "ok ", with: "")
-            return
         }
     }
-
+    
     func parsePingResult() {
         let jsonText = try? String(contentsOfFile: pingJsonFilePath, encoding: String.Encoding.utf8)
         guard let json = try? JSON(data: (jsonText ?? "").data(using: String.Encoding.utf8, allowLossyConversion: false)!) else {
             return
         }
-
+        
         var pingResHash: Dictionary<String, String> = [:]
         if json.arrayValue.count > 0 {
             for val in json.arrayValue {
@@ -149,12 +161,12 @@ class PingSpeed: NSObject {
                 pingResHash[name] = ping
             }
         }
-
+        
         let itemList = V2rayServer.list()
         if itemList.count == 0 {
             return
         }
-
+        
         for item in itemList {
             if !item.isValid {
                 continue
@@ -166,11 +178,11 @@ class PingSpeed: NSObject {
             }
         }
     }
-
+    
     func parseHost(item: V2rayItem) -> (String) {
         let cfg = V2rayConfig()
         cfg.parseJson(jsonText: item.json)
-
+        
         var host: String = ""
         var port: Int
         if cfg.serverProtocol == V2rayProtocolOutbound.vmess.rawValue {
@@ -186,7 +198,28 @@ class PingSpeed: NSObject {
             host = cfg.serverSocks5.servers[0].address
             port = Int(cfg.serverSocks5.servers[0].port)
         }
-
+        
         return host
+    }
+    
+    func runShell(launchPath: String, arguments: [String]) -> String? {
+        task = Process()
+        task?.launchPath = launchPath
+        task?.arguments = arguments
+        
+        let pipe = Pipe()
+        task?.standardOutput = pipe
+        task?.launch()
+        
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output = String(data: data, encoding: String.Encoding.utf8)!
+        
+        if output.count > 0 {
+            //remove newline character.
+            let lastIndex = output.index(before: output.endIndex)
+            return String(output[output.startIndex..<lastIndex])
+        }
+        
+        return output
     }
 }
