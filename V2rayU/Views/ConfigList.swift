48ea449c258f867c68996d8f3ae6f2a195626135
yanue <yanue@outlook.com>
2024-12-01 16:45:09 +0800

    列表 ui

--- V2rayU/Views/ConfigList.swift
+++ V2rayU/Views/ConfigList.swift
@@ -8,85 +8,135 @@
 import SwiftUI
 
 struct ConfigListView: View {
-    let list = [
-        ProxyModel(protocol: .trojan, address: "dss111", port: 443, id: "aaa", security: "auto2", remark: "test01"),
-        ProxyModel(protocol: .trojan, address: "dss222", port: 443, id: "bbbs", security: "auto1", remark: "test02"),
-    ]
-
+    @State private var list: [ProxyModel] = []
     @State private var sortOrder: [KeyPathComparator<ProxyModel>] = []
-    @State private var showDetails = false // Whether to show the detail view
     @State private var selection: Set<ProxyModel.ID> = []
+    @State private var selectedProxy: ProxyModel? = nil
+    @State private var selectGroup: GroupModel = defaultGroup
+    @State private var groups: [GroupModel] = []
     @State private var searchText = ""
-    @State private var selectedProxy: ProxyModel? = nil // For holding selected ProxyModel for detail view
 
-    var body: some View {
-        let filteredItems = list.filter { item in
-            searchText.isEmpty || item.address.lowercased().contains(searchText.lowercased()) ||
-                item.id.lowercased().contains(searchText.lowercased()) ||
-                item.remark.lowercased().contains(searchText.lowercased())
+    var filteredAndSortedItems: [ProxyModel] {
+        let filtered = list.filter { item in
+            (selectGroup.group == "" || selectGroup.group == item.subid) &&
+                (searchText.isEmpty || item.address.lowercased().contains(searchText.lowercased()) || item.remark.lowercased().contains(searchText.lowercased()))
         }
-
-        Table(filteredItems, selection: $selection, sortOrder: $sortOrder) {
-            TableColumn("Address", value: \.address)
-            TableColumn("Id", value: \.id)
-            TableColumn("Path", value: \.path)
-            TableColumn("Remark", value: \.remark)
-            TableColumn("Security", value: \.security)
+        .sorted(using: sortOrder)
+        // 循环增加序号
+        filtered.enumerated().forEach { index, item in
+            item.index = index
         }
-        .searchable(text: $searchText, prompt: "Search by Address, Id & Remark")
-        .contextMenu(forSelectionType: ProxyModel.ID.self) { _ in
-            // Add custom context menu items here if needed
-        } primaryAction: { items in
-            // This is executed when the row is double-clicked
-            if let selectedItem = items.first {
-                if let proxy = list.first(where: { $0.id == selectedItem }) {
-                    self.selectedProxy = proxy // Set the selected ProxyModel
-                    self.showDetails = true // Show the details view
-                    print("primaryAction", proxy.id, self.showDetails)
-                }
-            }
-        }
-
-        .onChange(of: sortOrder) { newOrder in
-            print("Sort order changed: \(newOrder)")
-        }
-
-        .onChange(of: showDetails) { newValue in
-            print("showDetails changed: \(newValue)")
-        }
-
-        .sheet(isPresented: $showDetails) {
-            if let proxy = selectedProxy {
-                VStack {
-                    Button("Close") {
-                        showDetails.toggle() // 关闭模态视图
-                    }.padding()
-                    ConfigView(item: proxy)
-                        .padding()
-                        .presentationDetents([.large])
-                   
-                }
-            }
-        }
+        return filtered
     }
-}
-
-struct ArticlesView: View {
-    @State var shouldPresentSheet = false
 
     var body: some View {
         VStack {
-            Button("Present Sheet") {
-                shouldPresentSheet.toggle()
+            HStack {
+                Picker("选择组", selection: $selectGroup) {
+                    ForEach(groups) { group in // 使用 groups 数组并遍历
+                        Text(group.name).tag(group as GroupModel) // 使用 .tag 来绑定选中的项
+                    }
+                }
+                .pickerStyle(MenuPickerStyle()) // 可根据需要选择不同的 Picker 样式
+                .padding()
+
+                Text("搜索")
+                TextField("Search by Address or Remark", text: $searchText)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                Button("刷新") {
+                    withAnimation {
+                        loadData()
+                    }
+                }
+                Button("Ping") {
+                    withAnimation {
+                    }
+                }
+
+                Button("删除") {
+                    withAnimation {
+                        list.removeAll { selection.contains($0.id) }
+                        selection.removeAll()
+                    }
+                }
+                .disabled(selection.isEmpty)
+
+                Button("新增") {
+                    withAnimation {
+                        let newProxy = ProxyModel(protocol: .trojan, address: "newAddress", port: 443, id: UUID().uuidString, security: "auto", remark: "New Remark")
+                        list.append(newProxy)
+                    }
+                }
             }
-            /// Present a sheet once `shouldPresentSheet` becomes `true`.
-            .sheet(isPresented: $shouldPresentSheet) {
-                print("Sheet dismissed!")
-            } content: {
+
+            Table(filteredAndSortedItems, selection: $selection, sortOrder: $sortOrder) {
+                TableColumn("#") { item in
+                    Text("\(item.index + 1)") // 显示 1-based 索引
+                }
+                .width(30)
+                TableColumn("Type", value: \.`protocol`.rawValue)
+                TableColumn("Remark", value: \.remark)
+                TableColumn("Address", value: \.address)
+                TableColumn("Port", value: \.port.description)
+                TableColumn("Network", value: \.network.rawValue)
+                TableColumn("TLS", value: \.streamSecurity.rawValue)
+            }
+            .contextMenu(forSelectionType: ProxyModel.ID.self) { items in
+                if let selectedItem = items.first {
+                    contextMenuProvider(item: selectedItem)
+                }
             }
         }
-        .padding()
-        .frame(width: 400, height: 300)
+        .sheet(item: $selectedProxy) { proxy in
+            VStack {
+                Button("Close") {
+                    // 如果需要关闭 `sheet`，将 `selectedProxy` 设置为 `nil`
+                    selectedProxy = nil
+                }
+                ConfigView(item: proxy)
+                    .padding()
+            }
+        }
+        .task {
+            loadData()
+        }
+    }
+
+    private func contextMenuProvider(item: ProxyModel.ID) -> some View {
+        Group {
+            if let proxy = list.first(where: { $0.id == item }) {
+                Button("Edit") {
+                    self.selectedProxy = proxy
+                }
+            }
+
+            Divider()
+
+            Button("Ping") {
+                // Handle ping action
+            }
+
+            Button("Action 3") {
+                // Handle another action
+            }
+        }
+    }
+
+    private func loadData() {
+        // Simulate data fetching
+        let fetchedData = [
+            ProxyModel(protocol: .trojan, address: "dss111", port: 443, id: "aaa", security: "auto2", remark: "testa01"),
+            ProxyModel(protocol: .trojan, address: "dss222", port: 443, id: "bbbs", security: "auto1", remark: "testb02"),
+        ]
+        let groupsData = [
+            defaultGroup,
+            GroupModel(name: "Yanue", group: "yanue"),
+        ]
+        withAnimation {
+            list = fetchedData
+            groups = groupsData
+        }
     }
 }
 
--- V2rayU/v2ray/V2rayProxy.swift
+++ V2rayU/v2ray/V2rayProxy.swift
@@ -52,7 +52,39 @@ import Cocoa
  */
 import SwiftUI
 
+
+class GroupModel: ObservableObject, Identifiable, Hashable {
+    static func == (lhs: GroupModel, rhs: GroupModel) -> Bool {
+       return lhs.group == rhs.group
+   }
+
+   func hash(into hasher: inout Hasher) {
+       hasher.combine(group) // 使用 group 来计算哈希值
+   }
+    
+    @Published var name: String = "全部"
+    @Published var group: String = ""
+    
+    enum CodingKeys: String, CodingKey {
+        case  name,group
+    }
+    
+    init(name:String,group:String){
+        self.name = name
+        self.group = group
+    }
+    
+     var id: String { // 提供唯一的标识符
+         return group
+     }
+
+     
+}
+
+@MainActor let defaultGroup = GroupModel(name: "全部", group: "")
+
 class ProxyModel: ObservableObject, Identifiable {
+    var index: Int = 0
     // 公共属性
     @Published var `protocol`: V2rayProtocolOutbound
     @Published var network: V2rayStreamNetwork = .tcp
